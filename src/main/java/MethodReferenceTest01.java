f7d30766038688b78931c9d445dbcc377c9112f7
lucaslopeslima95 <lucaslopesllima@gmail.com>
2022-03-15 13:14:41 -0300

    Usando Method References

--- src/main/java/MethodReferenceTest01.java
+++ src/main/java/MethodReferenceTest01.java
@@ -2,6 +2,9 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.function.BiPredicate;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class MethodReferenceTest01 {
 
@@ -10,5 +13,20 @@ public class MethodReferenceTest01 {
        List<Anime> animeList = new ArrayList<>(List.of(new Anime("Bersek",32),new Anime("Dragon Ball",50000),new Anime("Zorro",1000)));
         Collections.sort(animeList,AnimeComparators::compareByEpisodes);
         System.out.println(animeList);
+
+        AnimeComparators animeComparators = new AnimeComparators();
+        animeList.sort(animeComparators::compareByTitleNonStatic);
+        System.out.println(animeList);
+
+        ArrayList<String> list2 = new ArrayList<>(List.of("Zoilton","Zyekarosviky","Astrogildo","Perestroico","Sanderson"));
+        list2.sort(String::compareTo);
+        System.out.println(list2);
+
+        Function<String,Integer> numStringToInteger = Integer::parseInt;
+        System.out.println(numStringToInteger.apply("10"));
+
+        BiPredicate<List<String>,String>checkName = List::contains;
+        System.out.println(checkName.test(list2,"Zoilton"));
+
     }
 }
