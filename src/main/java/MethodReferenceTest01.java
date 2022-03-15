d59c825ccc6ce3733cf1da5814036ed23a47e82b
lucaslopeslima95 <lucaslopesllima@gmail.com>
2022-03-15 13:19:30 -0300

    Usando Method References

--- src/main/java/MethodReferenceTest01.java
+++ src/main/java/MethodReferenceTest01.java
@@ -10,10 +10,11 @@ public class MethodReferenceTest01 {
 
 
     public static void main(String[] args) {
+        //ordena por numerio
        List<Anime> animeList = new ArrayList<>(List.of(new Anime("Bersek",32),new Anime("Dragon Ball",50000),new Anime("Zorro",1000)));
         Collections.sort(animeList,AnimeComparators::compareByEpisodes);
         System.out.println(animeList);
-
+        //ordena por ordem alfabetica com metodo nao estatico
         AnimeComparators animeComparators = new AnimeComparators();
         animeList.sort(animeComparators::compareByTitleNonStatic);
         System.out.println(animeList);
@@ -21,12 +22,13 @@ public class MethodReferenceTest01 {
         ArrayList<String> list2 = new ArrayList<>(List.of("Zoilton","Zyekarosviky","Astrogildo","Perestroico","Sanderson"));
         list2.sort(String::compareTo);
         System.out.println(list2);
-
+        //converte string em int
         Function<String,Integer> numStringToInteger = Integer::parseInt;
         System.out.println(numStringToInteger.apply("10"));
-
+        //Busca por um item na lista
         BiPredicate<List<String>,String>checkName = List::contains;
         System.out.println(checkName.test(list2,"Zoilton"));
+
 
     }
 }
