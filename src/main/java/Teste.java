69df9ff731194e0cf04ea2b86efa212624abaac2
lucaslopeslima95 <lucaslopesllima@gmail.com>
2022-03-15 10:03:52 -0300

    Teste IntelliJ

--- src/main/java/Teste.java
+++ src/main/java/Teste.java
@@ -1,7 +1,8 @@
 public class Teste {
 
     public static void main(String[] args) {
-        System.out.println("teste");
-        System.out.println("teste 2");
+        System.out.println("TESTE");
+        System.out.println("TESTE 2");
+
     }
 }
