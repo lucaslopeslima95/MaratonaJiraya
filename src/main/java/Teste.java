6193bd81daa174f55bac8288ff1d544e7fb397d0
lucaslopeslima95 <lucaslopesllima@gmail.com>
2022-03-15 10:18:29 -0300

    Teste IntelliJ

--- src/main/java/Teste.java
+++ src/main/java/Teste.java
@@ -3,19 +3,6 @@ import javax.swing.*;
 public class Teste {
 
     public static void main(String[] args) {
-        System.out.print("TESTE");
-        System.out.print("TESTE 2");
-        System.out.print("TESTE 2");
-        System.out.print("TESTE 2");
-        System.out.print("TESTE 2");
-        System.out.print("TESTE 2");
-        System.out.print("TESTE 2");
-        System.out.print("TESTE 2");
-        System.out.print("TESTE 2");
-          System.out.print("TESTE 2");
-        System.out.print("TESTE 2");
-           System.out.print("TESTE 2");
-           System.out.print("TESTE 2");
 
     }
 }
